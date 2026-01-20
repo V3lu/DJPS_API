8db6d59e18f62cfe7f814c0c895f0760792ed7c5
Adam Wielogórski <71398159+Khangineer@users.noreply.github.com>
2026-01-20 18:40:54 +0100

    6401202026

--- Controllers/JobsController.cs
+++ Controllers/JobsController.cs
@@ -2,6 +2,9 @@
 
 namespace AJPS_API.Controllers
 {
+    public record Employee(int Id, string Name, int DepartmentId, decimal Salary);
+    public record Department(int Id, string DepartmentName);
+    public record DeptSalaryResult(int DeptId, decimal AvgSalary);
     [Route("api/[controller]")]
     [ApiController]
     public class JobsController : ControllerBase
@@ -29,5 +32,22 @@ namespace AJPS_API.Controllers
                 yield return i;
             }
         }
+
+        public string? Ret1(string? text)
+        {
+            return text?.ToUpper() ?? "Lack of data";
+        }
+            
+        public List<IGrouping<string?, Employee>> Ret2(List<Employee> employees)
+        {
+            return employees.GroupBy(eb => eb.Department).ToList();
+        }
+
+        public List<DeptSalaryResult> Ret3(List<Employee> employees)
+        {
+            return employees.GroupBy(eb => eb.DepartmentId).Select(eb => new DeptSalaryResult{ DeptId = eb.Key, AvgSalary = eb.Average(b => b.Salary)}).ToList();
+        }
+
+        public List<Employee> Ret4()
     }
 }
